27292bea173675d312cc674bfc36ba67fe38736a
tongdung121 <vojakja@gmail.com>
2025-08-22 15:02:02 +0700

    0o0

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,16 @@
 use App\Http\Controllers\TaiwindController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[TaiwindController::class, 'index'])->name('taiwind.index');
+Route::get('/', [TaiwindController::class, 'index'])->name('taiwind.index');
+
+Route::domain('storage.local')->group(function () {
+    Route::get('/', function () {
+        return 'Hello from storage.local';
+    });
+});
+
+Route::domain('api.local')->group(function () {
+    Route::get('/', function () {
+        return 'Hello from api.local';
+    });
+});
