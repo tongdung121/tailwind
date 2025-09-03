22c0aa20e54f0fbdf16fc3c815e7b8051cd0367f
tongdung121 <vojakja@gmail.com>
2025-09-03 10:43:40 +0700

    o0o

--- resources/views/taiwind/landing.blade.php
+++ resources/views/taiwind/landing.blade.php
@@ -12,18 +12,18 @@
 
 <body>
     <div id="fullpage">
-    <div class="section bg-blue-500 flex items-center justify-center">
-        <h1 class="text-5xl font-bold text-white">Section 1</h1>
-    </div>
+        <div class="section bg-blue-500 flex items-center justify-center">
+            <h1 class="text-5xl font-bold text-white">Section 1</h1>
+        </div>
 
-    <div class="section bg-green-500 flex items-center justify-center">
-        <h1 class="text-5xl font-bold text-white">Section 2</h1>
-    </div>
+        <div class="section bg-green-500 flex items-center justify-center">
+            <h1 class="text-5xl font-bold text-white">Section 2</h1>
+        </div>
 
-    <div class="section bg-red-500 flex items-center justify-center">
-        <h1 class="text-5xl font-bold text-white">Section 3</h1>
+        <div class="section bg-red-500 flex items-center justify-center">
+            <h1 class="text-5xl font-bold text-white">Section 3</h1>
+        </div>
     </div>
-</div>
 
 </body>
 
