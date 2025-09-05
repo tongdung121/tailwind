39fac25e3a8efd943b711ed3ba49c6eaad70a696
tongdung121 <vojakja@gmail.com>
2025-09-05 13:29:48 +0700

    o0o

--- resources/views/components/slide.blade.php
+++ resources/views/components/slide.blade.php
@@ -1,27 +1,29 @@
-<div class="swiper mySwiper w-full mx-auto">
-    <div class="swiper-wrapper">
-        @foreach ($slides as $item)
-            <div class="swiper-slide flex items-center justify-center w-full">
-                <div class="aspect-[1920/500]">
-                    <img src="{{ asset('uploads/slides/' . $item->image) }}" class="w-full h-full object-cover" />
+<div class="relative">
+    <div class="swiper mySwiper w-full mx-auto">
+        <div class="swiper-wrapper">
+            @foreach ($slides as $item)
+                <div class="swiper-slide flex items-center justify-center w-full">
+                    <div class="aspect-[1920/500]">
+                        <img src="{{ asset('uploads/slides/' . $item->image) }}" class="w-full h-full object-cover" />
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
 
-    <!-- Nút điều hướng -->
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-pagination"></div>
-</div>
-<!-- Ảnh nhỏ (thumbnail) -->
-<div class="swiper mySwiperThumbs w-full h-[100px]">
-    <div class="swiper-wrapper">
-        @foreach ($slides as $item)
-            <div class="swiper-slide w-[100px] cursor-pointer">
-                <img src="{{ asset('uploads/slides/' . $item->image) }}"
-                    class="w-full h-full object-cover rounded-md" />
-            </div>
-        @endforeach
+        <!-- Nút điều hướng -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+    <!-- Ảnh nhỏ (thumbnail) -->
+    <div class="swiper mySwiperThumbs h-[100px] !absolute bottom-2 left-0 right-0 mx-auto">
+        <div class="swiper-wrapper flex items-center justify-center gap-2">
+            @foreach ($slides as $item)
+                <div class="swiper-slide !w-[100px] !h-[100px] cursor-pointer border border-white rounded-md">
+                    <img src="{{ asset('uploads/slides/' . $item->image) }}"
+                        class="w-full h-full object-cover rounded-md" />
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
