0966f76f5d9bb50c61efa24a34a2f39b71b153e1
tongdung121 <vojakja@gmail.com>
2025-09-18 08:40:19 +0700

    0o0

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\SlideController;
 use App\Http\Controllers\TaiwindController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [TaiwindController::class, 'index'])->name('taiwind.index');
 Route::get('/landing1', [TaiwindController::class, 'landing'])->name('taiwind.landing');
 Route::get('/landing2', [TaiwindController::class, 'landing2'])->name('taiwind.landing2');
